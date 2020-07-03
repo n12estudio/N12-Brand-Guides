aec1e6430d4fece025d5793b7243afff6c4284ed
Alonso-Aldebaram <abraabramixx@gmail.com>
2020-07-03 03:38:20 -0500

    Primer hola mundo en React

---
+++ src/components/elements/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../style/Header.css';
+
+function Header() {
+  return (
+    <div className="Nav">
+      <header className="Nav-header">
+        <p>
+          Esto es el Header
+        </p>
+        </header>
+    </div>
+  );
+}
+
+export default Header;
