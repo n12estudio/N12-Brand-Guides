f3ab4225ae09e6ef6e9467850ab2d9326287c072
Alonso-Aldebaram <abraabramixx@gmail.com>
2020-07-06 05:02:28 -0500

    Menu, portada y secciones creadas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import logo from '../images/logo.svg';
 import '../style/Home.css';
 
-function HolaMundo(nombre, edad){
-  var presentacion = (<div>
-    <h2>Hola, soy {nombre}</h2>
-    <h3>Tengo {edad} años</h3>
-  </div>
-  );
-  return presentacion
-}
+//Importar componentes
+import Header from '../components/sections/Header';
 
 function App() {
-  var nombre = "Victor Abraham"
-  
     return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo esto es el Header.
-        </p>
-{HolaMundo(nombre, 26)}
+       <header id="inicio">
+       <Header />
       </header>
     </div>
+      <section id="guias">
+        <div id="heading"></div>
+      </section>
+      <section id="recursos">
+        <div id="heading"></div>
+      </section>
+    </>
   );
 }
 
