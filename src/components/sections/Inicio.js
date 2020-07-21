15331534d43f045058e37e9c7eeab4c4392efc0b
Luis Casillas <luiscasillas@n12.mx>
2020-07-21 12:33:02 -0500

    style update, review in progress

--- src/components/sections/Inicio.js
+++ src/components/sections/Inicio.js
@@ -1,30 +1,45 @@
-import React from 'react';
-import LogoPort from '../../images/Yoon Lolen.png'
-import { Container, Row, Col, Button } from 'reactstrap';
-import '../../style/Inicio.css';
-import Navegacion from '../elements/Navegacion';
+import React from "react";
+import LogoPort from "../../images/Yoon Lolen.png";
+import { Container, Row, Col, Button } from "reactstrap";
+import "../../style/Inicio.css";
+import Navegacion from "../elements/Navegacion";
 
 function Inicio() {
   return (
-      <header id="inicio"> 
-      <Navegacion/>
-      <Container>        
+    <header id="inicio">
+      <Navegacion />
+      <Container fluid>
         <Row sm="1" md="1" className="Inicio">
           <Col sm="12" md="12">
             <Row xs="1" sm="1" md="1">
-              <Col  className="contini"><img src={LogoPort} href="#App-header" className="portLogo" alt="logoPort" /></Col>
+              <Col className="contini">
+                <img
+                  src={LogoPort}
+                  href="#App-header"
+                  className="portLogo"
+                  alt="logoPort"
+                />
+              </Col>
               <Col className="txtini">
                 <h1 className="titulo">YOON LOLEN</h1>
                 <h2 className="subtitulo">Guías y recursos de marca</h2>
-                <Button outline color="primary" size="lg" className="btn-compartir" href="https://www.facebook.com/n12estudio">Compartir</Button>{' '}
+                <Button
+                  outline
+                  color="primary"
+                  size="lg"
+                  className="btn-compartir"
+                  href="https://www.facebook.com/n12estudio"
+                >
+                  Compartir
+                </Button>{" "}
                 <div class="flecha rebotante"></div>
               </Col>
             </Row>
-          </Col>       
+          </Col>
         </Row>
       </Container>
-      </header>
-    );
-  }
+    </header>
+  );
+}
 
-export default Inicio
+export default Inicio;
--- src/components/sections/Recursos.js
+++ src/components/sections/Recursos.js
@@ -1,36 +1,60 @@
-import React from 'react';
-import '../../style/Recursos.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDownload, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
-import { Container, Row, Col } from 'reactstrap';
-import Pdfn12 from '../../pdf/N12-Clientes.pdf'
+import React from "react";
+import "../../style/Recursos.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDownload,
+  faChevronCircleRight
+} from "@fortawesome/free-solid-svg-icons";
+import { Container, Row, Col } from "reactstrap";
+import Pdfn12 from "../../pdf/N12-Clientes.pdf";
 
 function Recursos() {
-    return (
-            <Container>
-                <Row sm="1" md="2" id="recursos">
-                    <Col sm="12" md="6" className="recurso1">
-                        <div class="txtrecur1">
-                        <h3>RECURSOS</h3>
-                        <a href="#recursos"><p><FontAwesomeIcon icon={faChevronCircleRight} className="frechader"/> 01 Logotipos</p></a>
-                        <a href="#recursos"><p><FontAwesomeIcon icon={faChevronCircleRight} className="frechader"/> 02 Texturas complementarias</p></a>
-                        </div>
-                    </Col>
-                    <Col sm="12" md="6" className="recurso2">
-                        <div class="txtrecur2">
-                        <h3>01</h3>
-                        <div class="txtdes1">
-                            <p>Logotipos</p>
-                            <a href={Pdfn12} download="N12 Clientes"><FontAwesomeIcon icon={faDownload} className="icondow2"/></a>
-                        </div>
-                        </div>
-                        <div class="zondes2">
-                        <iframe src={Pdfn12} title="title">
-                        </iframe>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
-export default Recursos
+  return (
+    <Container fluid>
+      <Row sm="1" md="2" id="recursos">
+        <Col sm="12" md="6" className="recurso1">
+          <div class="txtrecur1">
+            <h3>RECURSOS</h3>
+            <a href="#recursos">
+              <p>
+                <FontAwesomeIcon
+                  icon={faChevronCircleRight}
+                  className="frechader"
+                />{" "}
+                01 Logotipos
+              </p>
+            </a>
+            <a href="#recursos">
+              <p>
+                <FontAwesomeIcon
+                  icon={faChevronCircleRight}
+                  className="frechader"
+                />{" "}
+                02 Texturas complementarias
+              </p>
+            </a>
+          </div>
+        </Col>
+        <Col sm="12" md="6" className="recurso2">
+          <div class="txtrecur2">
+            <h3>01</h3>
+            <div class="txtdes1">
+              <p>Logotipos</p>
+              <a href={Pdfn12} download="N12 Clientes">
+                <FontAwesomeIcon icon={faDownload} className="icondow2" />
+              </a>
+            </div>
+          </div>
+          <div class="zondes2">
+            <embed
+              className="embed-responsive"
+              src={Pdfn12}
+              title="title"
+            ></embed>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+export default Recursos;
